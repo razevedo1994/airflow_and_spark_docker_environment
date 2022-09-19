a4ab5607ade1f6749d3a27affc1ac5c7f955ba2d
razevedo1994 <razevedo.contato@gmail.com>
2022-09-19 15:06:22 -0300

    Refactor build script

--- build_environment.sh
+++ build_environment.sh
@@ -2,5 +2,5 @@
 
 # Build the base images from which are based the Dockerfiles
 # then Startup all the containers at once
-docker build -t spark-base docker/spark/spark-base && \
-docker-compose up -d --build
+docker-compose up airflow-init && \
+docker-compose up -d
